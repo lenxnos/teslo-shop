75a1c6429c03903edec999cb7191623d6cd88a75
Nelson <nelson.soto.c05@gmail.com>
2023-01-06 17:41:49 -0400

    feat: add material ui - fonts, icons and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { lightTheme } from '../themes';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
