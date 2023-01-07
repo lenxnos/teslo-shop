19f7408223ddacd10d22a61735e4c4d6e0ada436
Nelson <nelson.soto.c05@gmail.com>
2023-01-07 01:11:56 -0400

    feat: add product slide show

---
+++ components/products/ProductSlideshow.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import styles from './ProductSlideshow.module.css';
+
+interface Props {
+  images: string[];
+}
+
+export function ProductSlideshow({ images }: Props) {
+  return (
+    <Slide
+      easing='ease'
+      duration={7000}
+      indicators
+    >
+      {
+        images.map((image) => {
+          const url = `/products/${image}`;
+          return (
+            <div className={styles['each-slide']} key={image}>
+              <div style={{
+                  backgroundImage: `url(${url})`,
+                  backgroundSize: 'cover',
+                }}></div>
+            </div>
+          )
+        })
+      }
+    </Slide>
+  )
+}
--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Grid, Typography } from '@mui/material';
 import { ShopLayout } from '../../components/layouts'
+import { ProductSlideshow } from '../../components/products';
 import { initialData } from '../../database/products'
 
 const product = initialData.products[0];
@@ -9,7 +10,7 @@ export default function ProductPage() {
     <ShopLayout title={product.title} pageDescription={product.description}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={7}>
-          {/* TODO: SlideShow */}
+          <ProductSlideshow images={product.images} />
         </Grid>
         <Grid item xs={12} sm={5}>
           <Box display="flex" flexDirection="column">
