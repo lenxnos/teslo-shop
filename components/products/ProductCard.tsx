11d9f57f80de2f533325fc90550eb96a21c3e3ec
Nelson <nelson.soto.c05@gmail.com>
2023-01-08 19:06:29 -0400

    feat: add a loading when load products page

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -10,6 +10,7 @@ interface Props {
 export function ProductCart({ product }: Props) {
 
   const [isHovered, setIsHovered] = useState(false);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   const productImage = useMemo(() => {
     return isHovered
@@ -34,13 +35,14 @@ export function ProductCart({ product }: Props) {
                 className="fadeIn"
                 image={productImage}
                 alt={product.title}
+                onLoad={() => setIsImageLoaded(true)}
               />
             </CardActionArea>
           </Link>
         </NextLink>
       </Card>
 
-      <Box sx={{ mt: 1 }} className='fadeIn'>
+      <Box sx={{ mt: 1, display: isImageLoaded ? 'block': 'none' }} className='fadeIn'>
         <Typography fontWeight={700}>{product.title}</Typography>
         <Typography fontWeight={500}>{`$${product.price}`}</Typography>
       </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,28 @@
 import { Typography } from '@mui/material';
 import { ShopLayout } from '../components/layouts';
 import { ProductList } from '../components/products';
-import { initialData } from '../database/products';
-import { IProduct } from '../interfaces';
+import { useProducts } from '../hooks';
+import { FullScreenLoading } from '../components/ui';
 
 export default function Home() {
+
+  const { products, isLoading } = useProducts('/products');
+
   return (
     <>
       <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores producto de calidad en Teslo Shop'}>
         <Typography variant="h1" component="h1">Tienda</Typography>
         <Typography variant="h2" sx={{ mb: 1 }}>Todos los productos</Typography>
 
-        <ProductList
-          products={initialData.products as IProduct[]}
-        />
+        {
+          isLoading
+            ? <FullScreenLoading />
+            : (
+              <ProductList
+                products={products}
+              />
+            )
+        }
       </ShopLayout>
     </>
   )
